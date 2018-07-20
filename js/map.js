981b44233dcc1851540ddf9f64e17f2e21491f6e
jenlyoung <jen.l.young@gmail.com>
2018-07-19 20:46:54 -0500

    Feat: add code for multiple markers and infowindows

--- js/map.js
+++ js/map.js
@@ -3,6 +3,7 @@ var map;
 //create markers array
 var markers = [];
 
+
 function initMap() {
 
     //set the center of the map
@@ -14,21 +15,40 @@ function initMap() {
         zoom: 15,
     });
 
-    var testLocation = {lat:38.58127490294978, lng: -90.62694820243829};
+    var infoWindow = new google.maps.InfoWindow();
 
-    var marker = new google.maps.Marker({
-        position: testLocation,
-        map: map,
-        title: "First Marker",
-        animation: google.maps.Animation.DROP
-    });
+    for (var i = 0; i < venues.length; i++) {
+        // Get the position from the location array.
+        var position = venues[i].location;
+        var title = venues[i].name;
+        // Create a marker per location, and put into markers array.
+        var marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: name,
+            animation: google.maps.Animation.DROP,
+            id: i
+        });
 
-    var infowindow = new google.maps.InfoWindow({
-        content: "It worked"
-    });
+        // Push the marker to our array of markers.
+        markers.push(marker);
+        // Create an onclick event to open an infoWindow at each marker.
+        marker.addListener('click', function () {
+            populateInfoWindow(this, infoWindow);
+        });
+    }
 
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
-    });
+    function populateInfoWindow(marker, infoWindow) {
+        // Check to make sure the infoWindow is not already opened on this marker.
+        if (infoWindow.marker != marker) {
+            infoWindow.marker = marker;
+            infoWindow.setContent('<div>' + marker.title + '</div>');
+            infoWindow.open(map, marker);
+            // Make sure the marker property is cleared if the infoWindow is closed.
+            infoWindow.addListener('closeclick', function () {
+                infoWindow.marker = null;
+            });
+        }
+    }
 }
 
